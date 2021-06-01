deeba074e0d31c625e866372f4d5518d5a09967c
root <danielulrichtamayo@gmail.com>
2021-05-31 20:08:46 -0500

    Update adding the text box for the special notes and modifying some tables (just design)

---
+++ Migrations/20210601010109_t2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TiendaEnLinea2.Migrations
+{
+    public partial class t2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SpecialNote",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SpecialNote",
+                table: "Orders");
+        }
+    }
+}
